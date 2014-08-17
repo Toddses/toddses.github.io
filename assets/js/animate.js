cfe7dfad77fab3afa3594b3b128c7ebba2ba5ea1
Todd Miller <toddses@gmail.com>
2014-08-17 00:22:44 -0400

    add album and slider functionality

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -11,7 +11,69 @@ jQuery(document).ready(function($) {
         $(".registry-wrap").slideToggle("slow");
     });
 
+    $("#album").click(function() {
+        $(".album-wrap").slideToggle("slow");
+    });
+
     $("#events").click(function() {
         $(".events-wrap").slideToggle("slow");
     });
+
+    function moveLeft() {
+        //if (!isRightMousedown) { return; }
+
+        var newPos = parseInt($(".album-list").css("left")) - 10;
+        $(".album-list").animate({"left": newPos}, "slow");
+            
+        //if (isRightMousedown) { moveLeft(); }
+    }
+
+    //$("#right-arrow").bind("mousedown", function() {
+        //var newPos = parseInt($(".album-list").css("left")) - 10;
+        //$(".album-list").animate({"left": newPos}, "slow");
+    //});
+
+    var $list = $(".album-list");
+    var listWidth = -5785;
+
+    function startScrollingLeft() {
+        // contintually increase scroll position, but don't go past 0
+        if (parseInt($list.css("left")) >= 0) {
+            $list.animate({"left": "0"}, 100, "linear");
+        } else {
+            $list.animate({"left": "+=50"}, 100, "linear", startScrollingLeft);
+        }
+    }
+    function startScrollingRight() {
+        // contintually increase scroll position, but don't go past the total width
+        if (parseInt($list.css("left")) <= listWidth) {
+            $list.animate({"left": listWidth}, 100, "linear");
+        } else {
+            $list.animate({"left": "-=50"}, 100, "linear", startScrollingRight);
+        }
+    }
+
+    function stopScrollingLeft() {
+        // stop increasing scroll position
+        $list.stop();
+    }
+    function stopScrollingRight() {
+        // stop increasing scroll position
+        $list.stop();
+    }
+
+    $("#left-arrow").mousedown(startScrollingLeft).mouseup(stopScrollingLeft);
+    $("#right-arrow").mousedown(startScrollingRight).mouseup(stopScrollingRight);
+
+    /*var isRightMousedown = false;
+    $("#right-arrow").mousedown(function() {
+        isRightMousedown = true;
+        moveLeft();
+    }).mouseup(function() {
+        isRightMousedown = false;
+    });*/
+    //$("#right-arrow").click(function() {
+        //var newPos = parseInt($(".album-list").css("left")) - 10;
+        //$(".album-list").animate({"left": newPos}, "slow");
+    //})
 });
