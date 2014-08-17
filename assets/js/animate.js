29a74a57e2ef260ae58eacb1b36caf044bf05cff
Todd Miller <toddses@gmail.com>
2014-08-17 00:57:53 -0400

    add baby shower info

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -22,42 +22,6 @@ jQuery(document).ready(function($) {
     var $list = $(".album-list");
     var listWidth = -5785;
 
-    /*function startScrollingLeft() {
-        // contintually increase scroll position, but don't go past 0
-        if (parseInt($list.css("left")) >= 0) {
-            $list.animate({"left": "0"}, 100, "linear");
-        } else {
-            $list.animate({"left": "+=50"}, 100, "linear", startScrollingLeft);
-        }
-    }
-    function startScrollingRight() {
-        // contintually increase scroll position, but don't go past the total width
-        if (parseInt($list.css("left")) <= listWidth) {
-            $list.animate({"left": listWidth}, 100, "linear");
-        } else {
-            $list.animate({"left": "-=50"}, 100, "linear", startScrollingRight);
-        }
-    }
-
-    function stopScrollingLeft() {
-        // stop increasing scroll position
-        $list.stop();
-    }
-    function stopScrollingRight() {
-        // stop increasing scroll position
-        $list.stop();
-    }
-
-    $("#left-arrow").mousedown(startScrollingLeft).mouseup(stopScrollingLeft);
-    $("#right-arrow").mousedown(startScrollingRight).mouseup(stopScrollingRight);
-
-    $("#right-arrow").on("touchstart", function() {
-        $list.animate({"left": "-=50"}, 100, "linear");
-    });
-    $("#left-arrow").on("touchstart", function() {
-        $list.animate({"left": "+=50"}, 100, "linear");
-    });*/
-
     $("#right-arrow a").click(function() {
         if (parseInt($list.css("left")) <= listWidth) {
             $list.animate({"left": listWidth}, 100, "linear");
